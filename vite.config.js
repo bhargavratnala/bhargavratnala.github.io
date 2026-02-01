f4db24700bfae95eac3bed9810329d4228c1b1e8
bhargavratnala <bhargavratnala2004@gmail.com>
2026-02-01 11:46:58 +0530

    added few keywords in index.html, added google verification file in build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
 import react from '@vitejs/plugin-react'
+import { globSync } from 'glob'
 
-// https://vitejs.dev/config/
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+// find all google*.html files
+const googleHtmlFiles = globSync('google*.html', { cwd: __dirname })
+
+// convert to Rollup input format
+const inputs = {
+  main: resolve(__dirname, 'index.html'),
+}
+
+googleHtmlFiles.forEach((file) => {
+  inputs[file.replace('.html', '')] = resolve(__dirname, file)
+})
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: inputs,
+    },
+  },
 })
